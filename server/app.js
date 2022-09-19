ec13507b0b7f3c89105c669c7a95db879fe17460
Ihor Sokolov <i.sokolov@geniusee.com>
2022-09-19 20:57:41 +0300

    basic implementation

---
+++ server/app.js
@@ -0,0 +1,39 @@
+require('dotenv').config();
+const express = require('express');
+const webpush = require('web-push');
+const { exchangeRate } = require('./currencies');
+
+const app = express();
+
+webpush.setVapidDetails(
+  'http://localhost:3000',
+  process.env.REACT_APP_WEB_PUSH_PUBLIC_KEY,
+  process.env.PRIVATE_KEY
+);
+
+app.use(express.json());
+
+const webPushSubscriptions = [];
+
+app.get('/', (_, res) => res.json(exchangeRate));
+
+app.put('/:currency', (req, res) => {
+  const { currency } = req.params;
+  const { value } = req.body;
+
+  exchangeRate[currency] = value;
+
+  webPushSubscriptions.forEach((subscription) => {
+    webpush.sendNotification(subscription, `Hello!!!!. New exchange rate is ${value}`);
+  });
+
+  res.json({ value });
+});
+
+app.post('/subscription', (req, res) => {
+  webPushSubscriptions.push(req.body);
+
+  res.sendStatus(201);
+});
+
+app.listen(process.env.SERVER_PORT, () => console.log('The app started'));
---
+++ server/currencies.js
@@ -0,0 +1,14 @@
+const currencies = {
+  USD: 'usd',
+  EUR: 'eur',
+};
+
+const exchangeRate = {
+  [currencies.EUR]: 37.09,
+  [currencies.USD]: 36.90,
+};
+
+module.exports = {
+  currencies,
+  exchangeRate,
+};
