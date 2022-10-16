0f227e3c4346f851fa936620cfcc78b707894015
Tetiana Matviienko <56842420+Tatiana-256@users.noreply.github.com>
2022-10-16 12:56:16 +0300

    Set up push notification (#2)
    * set up push notification
    
    * implement fixes
    
    * added axios
    
    * added checkboxes for the table
    
    * replace fetch request by axios
    
    * fix
    
    * uninstaled axios
    
    * removed todo
    
    * remove debug flag
    
    * edited checkboxes, fixed comments
    
    * fix codestyle
    
    * add blank line
    
    Co-authored-by: Ihor Sokolov <i.sokolov@geniusee.com>

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import CurrencyTable from '../Table';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Exchange rates</header>
+      <CurrencyTable />
+    </div>
+  );
+}
+
+export default App;
